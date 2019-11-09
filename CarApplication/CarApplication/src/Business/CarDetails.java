4972239700492565638c9746c16a9d27ee2b0920
SiddhiPrabhu1995 <57429405+SiddhiPrabhu1995@users.noreply.github.com>
2019-11-09 16:12:57 -0500

    Add files via upload

---
+++ CarApplication/CarApplication/src/Business/CarDetails.java
@@ -0,0 +1,162 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author Siddhi
+ */
+public class CarDetails
+{
+    
+    private String modelName;
+    private String price;
+    private String modelColor;
+    private String modelNumber;
+    private String modelType;
+    private String modelYear;
+    private String LicenseNumber;
+    private String carSpeed;
+    private String availabilityType;
+    private String carDetails;
+    private String feulType;
+    private String gearType;
+    private String warranty;
+    private String feulIntake;
+    private String numberOfSeats;
+    private String imagePath;
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getModelColor() {
+        return modelColor;
+    }
+
+    public void setModelColor(String modelColor) {
+        this.modelColor = modelColor;
+    }
+
+    public String getModelNumber() {
+        return modelNumber;
+    }
+
+    public void setModelNumber(String modelNumber) {
+        this.modelNumber = modelNumber;
+    }
+
+    public String getModelType() {
+        return modelType;
+    }
+
+    public void setModelType(String modelType) {
+        this.modelType = modelType;
+    }
+
+    public String getModelYear() {
+        return modelYear;
+    }
+
+    public void setModelYear(String modelYear) {
+        this.modelYear = modelYear;
+    }
+
+    public String getLicenseNumber() {
+        return LicenseNumber;
+    }
+
+    public void setLicenseNumber(String LicenseNumber) {
+        this.LicenseNumber = LicenseNumber;
+    }
+
+    public String getCarSpeed() {
+        return carSpeed;
+    }
+
+    public void setCarSpeed(String carSpeed) {
+        this.carSpeed = carSpeed;
+    }
+
+    public String getAvailabilityType() {
+        return availabilityType;
+    }
+
+    public void setAvailabilityType(String availabilityType) {
+        this.availabilityType = availabilityType;
+    }
+
+    public String getCarDetails() {
+        return carDetails;
+    }
+
+    public void setCarDetails(String carDetails) {
+        this.carDetails = carDetails;
+    }
+
+    public String getFeulType() {
+        return feulType;
+    }
+
+    public void setFeulType(String feulType) {
+        this.feulType = feulType;
+    }
+
+    public String getGearType() {
+        return gearType;
+    }
+
+    public void setGearType(String gearType) {
+        this.gearType = gearType;
+    }
+
+    public String getWarranty() {
+        return warranty;
+    }
+
+    public void setWarranty(String warranty) {
+        this.warranty = warranty;
+    }
+
+    public String getFeulIntake() {
+        return feulIntake;
+    }
+
+    public void setFeulIntake(String feulIntake) {
+        this.feulIntake = feulIntake;
+    }
+
+    public String getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public void setNumberOfSeats(String numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+
+
+    
+}
